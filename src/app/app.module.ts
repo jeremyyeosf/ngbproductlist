defdf5cccccd0c52106a92dc61a8ed30e8d33094
jeremyyeosf <jeremyyeo.sf@gmail.com>
2020-11-22 17:02:46 +0800

    finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { ProductRowComponent } from './product-row/product-row.component';
+import { ProductImageComponent } from './product-image/product-image.component';
+import { ProductDepartmentComponent } from './product-department/product-department.component';
+import { PriceDisplayComponent } from './price-display/price-display.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsListComponent,
+    ProductRowComponent,
+    ProductImageComponent,
+    ProductDepartmentComponent,
+    PriceDisplayComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
